b0434a6aeb8244c65aedc8da04b820086e9fcf34
jacobdlewis <jacobdlewis@gmail.com>
2015-02-04 14:57:33 -0600

    list items display correctly, and are styled

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,11 @@
 ;(function(){
   'use strict';
-  var pricesUrl = "http://lorempixel.com/200/200/animals/";
-  console.log("hello world");
-
-  $.get(pricesUrl, format(data), json{
-    console.log(response);
+  var pricesUrl = "https://savingsmultipliedssh.firebaseio.com/items.json";
+  $.get(pricesUrl, function(data){
+    _.forEach(data, function(item){
+      var $itemForSale = $('<div class="container"><img class="item_image" src="' + item.image + '"><div>' + item.title + '</div><div>' + item.price + '</div><div>' + item.seller + '</div><div>' + item.endDate + '</div></div>');
+      $('.itemGroup').append($itemForSale);
+    });
   });
-  function format(data){
 
-  };
 }());
