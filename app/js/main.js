8dc274ca67f03df27545615916783dd07e4a6749
jacobdlewis <jacobdlewis@gmail.com>
2015-02-04 15:37:40 -0600

    cleaned linting errors, and installed jquery, lodash, and moment as bower dependencies

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,22 @@
+/* jshint jquery: true */
+/* global _: false */
+
 ;(function(){
   'use strict';
-  var pricesUrl = "https://savingsmultipliedssh.firebaseio.com/items.json";
+  var pricesUrl = 'https://savingsmultipliedssh.firebaseio.com/items.json';
   $.get(pricesUrl, function(data){
     _.forEach(data, function(item){
-      var $itemForSale = $('<div class="container"><img class="item_image" src="' + item.image + '"><div>' + item.title + '</div><div>' + item.price + '</div><div>' + item.seller + '</div><div>' + item.endDate + '</div></div>');
+      var $itemForSale = $('<div class="container"><img class="item_image" src="' +
+                           item.image +
+                           '"><div>' +
+                           item.title +
+                           '</div><div>' +
+                           item.price +
+                           '</div><div>' +
+                           item.seller +
+                           '</div><div>' +
+                           item.endDate +
+                           '</div></div>');
       $('.itemGroup').append($itemForSale);
     });
   });
