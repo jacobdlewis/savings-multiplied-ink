54b8b1637199554bbc75f1a0b8cbb738c43c810a
jacobdlewis <jacobdlewis@gmail.com>
2015-02-04 21:51:09 -0600

    dead images are handled appropriately

--- app/js/main.js
+++ app/js/main.js
@@ -6,21 +6,29 @@
   var pricesUrl = 'https://savingsmultipliedssh.firebaseio.com/items.json';
   $.get(pricesUrl, function(data){
     _.forEach(data, function(item){
-      var itemImg = item.image;
-      if (!itemImg) {
-        itemImg = 'http://i.imgur.com/fyLXpT9.jpg';
-      }
-      var $itemForSale = $('<div class="container"><img class="item_image" src="' +
-                           itemImg +
-                           '"><div>' +
-                           item.title +
-                           '</div><div>' +
-                           item.price +
-                           '</div><div>' +
-                           item.seller +
-                           '</div><div>' +
-                           item.endDate +
-                           '</div></div>');
+      //variables from returned data
+      var image = item.image;
+      var title = item.title;
+      var price = item.price;
+      var seller = item.seller;
+      var endDate = item.endDate;
+      //jquery variables to append to page
+      var $image = $('<img class="item_image" src='+ image +'>')
+       .error(function(){
+         $image.attr( 'src', 'img/deadlink.png');
+       });
+      var $title = $('<div>' + title + '</div>');
+      var $price = $('<div>' + price + '</div>');
+      var $seller = $('<div>' + seller + '</div>');
+      var $endDate = $('<div>' + endDate + '</div>');
+      var $itemForSale = $('<div></div>')
+        .attr('class', 'item_container');
+
+      $itemForSale.append($image)
+        .append($title)
+        .append($price)
+        .append($seller)
+        .append($endDate);
       $('.itemGroup').append($itemForSale);
     });
   });
